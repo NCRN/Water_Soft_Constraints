b2838af1a4322e8139f8f2a984c9bac58af5ee11
John Paul Schmit <jschmit@nps.gov>
2023-12-18 14:23:45 -0500

    v 0.01 First commit
    - Mostly a commented outline of code

---
+++ Water_Soft_Constraints.R
@@ -0,0 +1,51 @@
+#### Load Libraries ####
+
+library(tidyverse)
+library(brms)
+library(tidybayes)
+
+
+
+#### Read in Data ####
+
+# data should be in a /Data directory
+# need hard constraints and the full data set. 
+
+#### Create Vectors that store which sites and characteristics we want ####
+
+Site_Names <- NA
+Char_Names <- NA
+
+#### Filter data and format to what we want ####
+
+
+
+#### Define functions ####
+
+##### Analysis function #####
+
+
+##### Constraint Extraction function #####
+
+
+
+#### Run Functions ####
+
+###### Figure out which data sets have enough data #####
+
+##### run analysis #####
+
+##### extract contstraints #####
+
+##### Do whatever fiddly stuff is needed for sites / characteristics with low data #####
+
+
+
+
+#### Export Results ####
+
+##### get data into correct format #####
+
+###### export #####
+
+
