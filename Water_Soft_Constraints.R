a8ea286f4f744a2b720cdcfc2fc23c7cb7ef1888
Danmyers901 <71978159+Danmyers901@users.noreply.github.com>
2023-12-19 11:17:10 -0500

    Update Water_Soft_Constraints.R
    Added reading data, formatting export, and exporting SoftConstraints table.

--- Water_Soft_Constraints.R
+++ Water_Soft_Constraints.R
@@ -17,6 +17,19 @@ library(ggpubr)
 # data should be in a /Data directory
 # need hard constraints and the full data set. 
 
+# Read data
+fileName <- "20231128_wqp_wqx_bss_wq_npsncrn.csv" 
+Water_Data <- read_csv(paste("Data/", fileName, sep=""))
+
+# Read Lookup and constraints
+lookup <- read.csv("Data/Lookup.csv")
+softConstraints <- read.csv("Data/SoftConstraints 2.csv")
+hardConstraints <- read.csv("Data/HardConstraints.csv")
+
+# Select WQ data by project identifier
+Water_Data <- Water_Data[Water_Data$ProjectIdentifier=="USNPS NCRN Perennial stream water monitoring",]
+
+
 #### Create Vectors that store which sites and characteristics we want ####
 
 Site_Names <- NA
@@ -32,7 +45,7 @@ Char_Names <- NA
 
 
 ##### Constraint Extraction function #####
-
+source("SoftConstraintsFunc.R")
 
 
 #### Run Functions ####
@@ -51,7 +64,14 @@ Char_Names <- NA
 #### Export Results ####
 
 ##### get data into correct format #####
+# Get soft characteristic column names
+soft_names <- lookup$SoftConstraints_name[lookup$CharacteristicName==Char_Names]
+
+# Fill matching cells in softCharacteristics
+softConstraints[softConstraints$Year==2023 &
+                  softConstraints$Location_ID==Site_Names, 
+                soft_names] <- Soft_Limits[,2:3]
 
 ###### export #####
-
+write.csv(softConstraints,"SoftConstraints-Filled.csv",row.names = F,quote=F)
 
