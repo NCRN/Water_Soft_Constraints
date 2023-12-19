537493b429bfa52ed2fa80770a99d3ede0fb8963
Danmyers901 <71978159+Danmyers901@users.noreply.github.com>
2023-12-19 13:54:33 -0500

    Update Water_Soft_Constraints.R
    Added to have a minimum number of records needed to create a model, otherwise it just skips it.

--- Water_Soft_Constraints.R
+++ Water_Soft_Constraints.R
@@ -53,6 +53,9 @@ Char_Names <- c("Temperature, water", "Barometric pressure", "Conductivity",
 # Do you want to make plots?
 To_Plot = 0 # 1=yes, 0=no
 
+# What is the minimum number of records we want to make a model?
+min_records <- 3
+
 # Start ticker
 ticker <- 1
 
@@ -60,63 +63,70 @@ ticker <- 1
 for (i in 1:length(Site_Names)){
   for (j in 1:length(Char_Names)){
     for (k in 1:length(Model_Year)){
+      
+      # Check if there's enough data to be worth it
+      n_records <- nrow(Water_Data[Water_Data$MonitoringLocationIdentifier==Site_Names[i] &
+                                Water_Data$CharacteristicName==Char_Names[j] &
+                                as.numeric(year(Water_Data$ActivityStartDate))==Model_Year[k],])
+      
+      if (n_records >= min_records){ # Arbitrary number, below which we would not want to make a model
   
-      #### Filter data and format to what we want ##############################
-      # Select the desired distribution of the data
-      if (Char_Names[j] %in% c(
-        "Acid Neutralizing Capacity (ANC)", "Temperature, air", "Wetted Width", 
-        "Cross-Section Depth", "RBP Stream Velocity", "Temperature, water",
-        "pH", "Dissolved oxygen saturation", "Dissolved oxygen (DO)"
-      )){
-        distribution <- "gaussian"
+        #### Filter data and format to what we want ##############################
+        # Select the desired distribution of the data
+        if (Char_Names[j] %in% c(
+          "Acid Neutralizing Capacity (ANC)", "Temperature, air", "Wetted Width", 
+          "Cross-Section Depth", "RBP Stream Velocity", "Temperature, water",
+          "pH", "Dissolved oxygen saturation", "Dissolved oxygen (DO)"
+        )){
+          distribution <- "gaussian"
+          
+        } else if (Char_Names[j] %in% c(
+          "Base flow discharge", "Specific conductance", "Salinity", "Conductivity", 
+          "Solids, Dissolved (TDS)", "Total Nitrogen, mixed forms", "Nitrogen, ammonia (NH3) as NH3",
+          "Total Phosphorus, mixed forms", "Phosphorus, orthophosphate as PO4",
+          "Chlorine", "Turbidity"
+        )){
+          distribution <- "gamma"
+          
+        } else {distribution=="gaussian"} # In case something new pops up
         
-      } else if (Char_Names[j] %in% c(
-        "Base flow discharge", "Specific conductance", "Salinity", "Conductivity", 
-        "Solids, Dissolved (TDS)", "Total Nitrogen, mixed forms", "Nitrogen, ammonia (NH3) as NH3",
-        "Total Phosphorus, mixed forms", "Phosphorus, orthophosphate as PO4",
-        "Chlorine", "Turbidity"
-      )){
-        distribution <- "gamma"
         
-      } else {distribution=="gaussian"} # In case something new pops up
-      
-      
-      #### Define functions ####################################################
-      
-      ##### Analysis function #####
-      
-      
-      ##### Constraint Extraction function #####
-      source("SoftConstraintsFunc.R")
-      
-      
-      #### Run Functions #######################################################
-      Soft_Limits <- SoftConstraintsFunc(Water_Data, Site_Names[i], Char_Names[j], distribution, Model_Year[k], To_Plot)
-      
-      ###### Figure out which data sets have enough data #####
-      
-      ##### run analysis #####
-      
-      ##### extract contstraints #####
-      
-      ##### Do whatever fiddly stuff is needed for sites / characteristics with low data #####
-      
-      
-      #### Export Results ######################################################
-      
-      ##### get data into correct format #####
-      # Get soft characteristic column names
-      soft_names <- lookup$SoftConstraints_name[lookup$CharacteristicName==Char_Names[j]]
-      
-      # Fill matching cells in softCharacteristics
-      softConstraints[softConstraints$Year==Model_Year[k] &
-                        softConstraints$Location_ID==Site_Names[i], 
-                      soft_names] <- Soft_Limits[,2:3]
+        #### Define functions ####################################################
+        
+        ##### Analysis function #####
+        
+        
+        ##### Constraint Extraction function #####
+        source("SoftConstraintsFunc.R")
+        
+        
+        #### Run Functions #######################################################
+        Soft_Limits <- SoftConstraintsFunc(Water_Data, Site_Names[i], Char_Names[j], distribution, Model_Year[k], To_Plot)
+        
+        ###### Figure out which data sets have enough data #####
+        
+        ##### run analysis #####
+        
+        ##### extract contstraints #####
+        
+        ##### Do whatever fiddly stuff is needed for sites / characteristics with low data #####
+        
+        
+        #### Export Results ######################################################
+        
+        ##### get data into correct format #####
+        # Get soft characteristic column names
+        soft_names <- lookup$SoftConstraints_name[lookup$CharacteristicName==Char_Names[j]]
+        
+        # Fill matching cells in softCharacteristics
+        softConstraints[softConstraints$Year==Model_Year[k] &
+                          softConstraints$Location_ID==Site_Names[i], 
+                        soft_names] <- Soft_Limits[,2:3]
+      }
       
       # Print progress
       print(paste("Finished", ticker, "out of", (length(Site_Names)*length(Char_Names)*length(Model_Year))))
       ticker <- ticker + 1
-
     }
   }
 }
