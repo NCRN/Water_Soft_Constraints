5cd7f18f32ce02faa67d5cf31cdce0dd11f29490
Danmyers901 <71978159+Danmyers901@users.noreply.github.com>
2023-12-19 11:40:52 -0500

    Update Water_Soft_Constraints.R
    Added automatically selecting distributions

--- Water_Soft_Constraints.R
+++ Water_Soft_Constraints.R
@@ -31,12 +31,33 @@ Water_Data <- Water_Data[Water_Data$ProjectIdentifier=="USNPS NCRN Perennial str
 
 
 #### Create Vectors that store which sites and characteristics we want ####
+# All of them
+Site_Names <- unique(Water_Data$MonitoringLocationIdentifier)
+Char_Names <- unique(Water_Data$CharacteristicName)
 
-Site_Names <- NA
-Char_Names <- NA
+# A sample
+Site_Names <- "NCRN_GWMP_MIRU"
+Char_Names <- "Specific conductance"
 
 #### Filter data and format to what we want ####
+# Select the desired distribution of the data
+if (Char_Names %in% c(
+  "Acid Neutralizing Capacity (ANC)", "Temperature, air", "Wetted Width", 
+  "Cross-Section Depth", "RBP Stream Velocity", "Temperature, water",
+  "pH", "Dissolved oxygen saturation", "Dissolved oxygen (DO)"
+)){
+  distribution <- "gaussian"
+} else if (Char_Names %in% c(
+  "Base flow discharge", "Specific conductance", "Salinity", "Conductivity", 
+  "Solids, Dissolved (TDS)", "Total Nitrogen, mixed forms", "Nitrogen, ammonia (NH3) as NH3",
+  "Total Phosphorus, mixed forms", "Phosphorus, orthophosphate as PO4",
+  "Chlorine", "Turbidity"
+)){
+  distribution <- "gamma"
+} else {distribution=="gaussian"} # In case something new pops up
 
+# Select year
+Model_Year <- 2023
 
 
 #### Define functions ####
@@ -49,6 +70,7 @@ source("SoftConstraintsFunc.R")
 
 
 #### Run Functions ####
+Soft_Limits <- SoftConstraintsFunc(Water_Data, Site_Names, Char_Names, distribution, Model_Year)
 
 ###### Figure out which data sets have enough data #####
 
@@ -68,7 +90,7 @@ source("SoftConstraintsFunc.R")
 soft_names <- lookup$SoftConstraints_name[lookup$CharacteristicName==Char_Names]
 
 # Fill matching cells in softCharacteristics
-softConstraints[softConstraints$Year==2023 &
+softConstraints[softConstraints$Year==Model_Year &
                   softConstraints$Location_ID==Site_Names, 
                 soft_names] <- Soft_Limits[,2:3]
 
